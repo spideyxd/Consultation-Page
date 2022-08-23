2a077b29e6f5ae4574787cf1453df06f659c02e5
Shivam Negi <shivam2612002@gmail.com>
2022-08-23 23:57:52 +0530

    form calendly integrated

--- src/components/FormAnt.js
+++ src/components/FormAnt.js
@@ -1,17 +1,68 @@
-import React from 'react';
+import { Button, message, Steps } from 'antd';
 import 'antd/dist/antd.css';
-import {DollarOutlined ,LoadingOutlined, SmileOutlined, SolutionOutlined, UserOutlined } from '@ant-design/icons';
-import { Steps } from 'antd';
+import React, { useState } from 'react';
+import Tryin from './Tryin';
+import Slot from './Slot';
 
 const { Step } = Steps;
+const steps = [
+  {
+    title: 'First',
+    content: <Tryin/>,
+  },
+  {
+    title: 'Second',
+    content:<Slot/> ,
+  },
+  {
+    title: 'Last',
+    content:  'ok',
+  },
+];
 
-const FormAnt = () => (
-  <Steps>
-    <Step status="wait" title="Login" icon={<UserOutlined />} />
-    <Step status="wait" title="Verification" icon={<SolutionOutlined />} />
-    <Step status="wait" title="Pay" icon={<DollarOutlined />} />
-    <Step status="wait" title="Done" icon={<SmileOutlined />} />
-  </Steps>
-);
+const FormAnt = () => {
+  const [current, setCurrent] = useState(0);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
+  return (
+    <>
+      <Steps current={current}>
+        {steps.map((item) => (
+          <Step key={item.title} title={item.title} />
+        ))}
+      </Steps>
+      <div className="steps-content">{steps[current].content}</div>
+      <div className="steps-action">
+        {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Next
+          </Button>
+        )}
+        {current === steps.length - 1 && (
+          <Button type="primary" onClick={() => message.success('Processing complete!')}>
+            Done
+          </Button>
+        )}
+        {current > 0 && (
+          <Button
+            style={{
+              margin: '0 8px',
+            }}
+            onClick={() => prev()}
+          >
+            Previous
+          </Button>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default FormAnt;
---
+++ src/components/Slot.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { InlineWidget } from "react-calendly";
+
+const Slot = () => {
+  return (
+    <div className="Slot">
+      <InlineWidget pageSettings={{
+  backgroundColor: 'ffffff',
+  hideEventTypeDetails: false,
+  hideLandingPageDetails: false,
+  primaryColor: '00a2ff',
+  textColor: '4d5055'
+}}   url="https://calendly.com/astroanalytics/consultation-booking" />
+    </div>
+  );
+};
+
+export default Slot;
